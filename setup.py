10518e10a2e6cc26f3da657a7bc44148fea34e10
miroslav14 <lukacmiroslav@yahoo.co.uk>
2019-09-19 16:29:20 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='qtpi_kernel',
+    version='1.1',
+    packages=['qtpi_kernel'],
+    description='Simple example of qtpi kernel python wrapper for Jupyter',
+    long_description=readme,
+    author='Jupyter Development Team',
+    author_email='jupyter@googlegroups.com',
+    url='https://github.com/jupyter/echo_kernel',
+    install_requires=[
+        'jupyter_client', 'IPython', 'ipykernel'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ],
+)
