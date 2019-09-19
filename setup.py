47fd6a8e93c7f095574f58de7ff595101ce72b90
miroslav14 <lukacmiroslav@yahoo.co.uk>
2019-09-20 00:11:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup
-#from distutils.core import setup
+#from setuptools import setup
+from distutils.core import setup
 
 with open('README.rst') as f:
     readme = f.read()
